7a2127a085c0d4b49de64b8931d1758adb3076f0
Christopher Perry <mr.christopher.perry@gmail.com>
2020-07-14 22:22:03 -0700

    example of working with sprite sheet

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
 #include "resources.h"
 #include "cleanup.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+//We'll just be using square tiles for now
+const int TILE_SIZE = 40;
 
 /**
 * Log an SDL error with some error message to the output stream of our choice
@@ -16,29 +19,37 @@ void logSDLError(std::ostream &os, const std::string &msg) {
 }
 
 /**
-* Loads a BMP image into a texture on the rendering device
-* @param file The BMP image file to load
+* Loads an image into a texture on the rendering device
+* @param file The image file to load
 * @param ren The renderer to load the texture onto
 * @return the loaded texture, or nullptr if something went wrong.
 */
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
-	//Initialize to nullptr to avoid dangling pointer issues
-	SDL_Texture *texture = nullptr;
-	//Load the image
-	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
-	//If the loading went ok, convert to texture and return the texture
-	if (loadedImage != nullptr) {
-		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
-		SDL_FreeSurface(loadedImage);
-		//Make sure converting went ok too
-		if (texture == nullptr) {
-			logSDLError(std::cout, "CreateTextureFromSurface");
-		}
-	}
-	else {
-		logSDLError(std::cout, "LoadBMP");
+	SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
+	if (texture == nullptr) {
+		logSDLError(std::cout, "LoadTexture");
 	}
 	return texture;
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+* width and height
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param x The x coordinate to draw to
+* @param y The y coordinate to draw to
+* @param w The width of the texture to draw
+* @param h The height of the texture to draw
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h) {
+	//Setup the destination rectangle to be at the position we want
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	dst.w = w;
+	dst.h = h;
+	SDL_RenderCopy(ren, tex, NULL, &dst);
 }
 
 /**
@@ -50,19 +61,62 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
 * @param y The y coordinate to draw to
 */
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y) {
-	//Setup the destination rectangle to be at the position we want
+	int w, h;
+	SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+	renderTexture(tex, ren, x, y, w, h);
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at some destination rect
+* taking a clip of the texture if desired
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param dst The destination rectangle to render the texture to
+* @param clip The sub-section of the texture to draw (clipping rect)
+*		default of nullptr draws the entire texture
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst, SDL_Rect *clip = nullptr)
+{
+	SDL_RenderCopy(ren, tex, clip, &dst);
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+* the texture's width and height and taking a clip of the texture if desired
+* If a clip is passed, the clip's width and height will be used instead of
+*	the texture's
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param x The x coordinate to draw to
+* @param y The y coordinate to draw to
+* @param clip The sub-section of the texture to draw (clipping rect)
+*		default of nullptr draws the entire texture
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip = nullptr)
+{
 	SDL_Rect dst;
 	dst.x = x;
 	dst.y = y;
-	//Query the texture to get its width and height to use
-	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	SDL_RenderCopy(ren, tex, NULL, &dst);
+	if (clip != nullptr) {
+		dst.w = clip->w;
+		dst.h = clip->h;
+	}
+	else {
+		SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	}
+	renderTexture(tex, ren, dst, clip);
 }
 
 int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		logSDLError(std::cout, "SDL_Init");
+		return 1;
+	}
+
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
+		logSDLError(std::cout, "IMG_Init");
+		SDL_Quit();
 		return 1;
 	}
 
@@ -82,33 +136,69 @@ int main(int argc, char* argv[])
 	}
 
 	const std::string resourcePath = getResourcePath();
-	SDL_Texture* background = loadTexture(resourcePath + "background.bmp", renderer);
-	SDL_Texture* image = loadTexture(resourcePath + "image.bmp", renderer);
-	if (background == nullptr || image == nullptr) {
-		cleanup(background, image, renderer, window);
+	SDL_Texture* image = loadTexture(resourcePath + "circles.png", renderer);
+	if (image == nullptr) {
+		cleanup(image, renderer, window);
+		IMG_Quit();
 		SDL_Quit();
 		return 1;
 	}
 
-	SDL_RenderClear(renderer);
-
-	int bW, bH;
-	SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
-	renderTexture(background, renderer, 0, 0);
-	renderTexture(background, renderer, bW, 0);
-	renderTexture(background, renderer, 0, bH);
-	renderTexture(background, renderer, bW, bH);
-
-	int iW, iH;
-	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+	//iW and iH are the clip width and height
+    //We'll be drawing only clips so get a center position for the w/h of a clip
+	int iW = 100, iH = 100;
 	int x = SCREEN_WIDTH / 2 - iW / 2;
 	int y = SCREEN_HEIGHT / 2 - iH / 2;
-	renderTexture(image, renderer, x, y);
 
-	SDL_RenderPresent(renderer);
-	SDL_Delay(1000);
+	//Setup the clips for our image
+	SDL_Rect clips[4];
+	for (int i = 0; i < 4; ++i) {
+		clips[i].x = i / 2 * iW;
+		clips[i].y = i % 2 * iH;
+		clips[i].w = iW;
+		clips[i].h = iH;
+	}
+	//Specify a default clip to start with
+	int useClip = 0;
 
-	cleanup(background, image, renderer, window);
+	//Our event structure
+	SDL_Event e;
+	bool quit = false;
+	while (!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT)
+				quit = true;
+			//Use number input to select which clip should be drawn
+			if (e.type == SDL_KEYDOWN) {
+				switch (e.key.keysym.sym) {
+				case SDLK_1:
+					useClip = 0;
+					break;
+				case SDLK_2:
+					useClip = 1;
+					break;
+				case SDLK_3:
+					useClip = 2;
+					break;
+				case SDLK_4:
+					useClip = 3;
+					break;
+				case SDLK_ESCAPE:
+					quit = true;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		//Render the scene
+		SDL_RenderClear(renderer);
+		renderTexture(image, renderer, x, y, &clips[useClip]);
+		SDL_RenderPresent(renderer);
+	}
+
+	cleanup(image, renderer, window);
+	IMG_Quit();
 	SDL_Quit();
 
 	return 0;
